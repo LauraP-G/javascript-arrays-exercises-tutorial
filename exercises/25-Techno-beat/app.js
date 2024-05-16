8d08d28fea4cc30d6e7bdf71a373722939c2c21b
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-05-16 10:01:28 +0000

    ejercicios finalizados

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,4 +1,29 @@
 // Your code here
+const lyricsGenerator= (arr) =>{
+    let musicDj = "";
+    let cero = "Boom";
+    let uno = "Drop the bass"
+    let tripleUno = "!!!Break the bass!!!"
+    let contador = 0;
+    
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 0){
+            musicDj += cero + " ";
+        }
+        else if (arr[i] === 1){
+            musicDj += uno + " ";
+            contador++;
+
+                if(contador == 3) {
+                musicDj += tripleUno + " ";
+                contador = 0;
+            }
+        }
+
+    }
+
+    return musicDj
+}
 
 
 // Don't change anything below this line
